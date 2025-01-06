a513daf032b19ca67d2d7404099c8ec770b72f6d
alesscol7 <alessia.columban@student.howest.be>
2025-01-06 16:49:09 +0100

    commit again videos

--- Athletes/KeypointDetection/try3.py
+++ Athletes/KeypointDetection/try3.py
@@ -6,8 +6,8 @@ from ultralytics import YOLO
 model = YOLO("yolov8s-pose.pt")  # Replace with your model path if different
 
 # Define the path to the video file and output folder
-video_path = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/YouTubeDownload/DownloadedVideos/Film techniek atletiek.mp4"
-output_video_folder = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/KeypointDetection/cutvideos/Film techniek atletiek"
+video_path = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/YouTubeDownload/DownloadedVideos/opdracht atletiek.mp4"
+output_video_folder = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/KeypointDetection/cutvideos/opdracht atletiek"
 # Ensure output folder exists
 os.makedirs(output_video_folder, exist_ok=True)
 
