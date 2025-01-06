68ce615071af806687e1d7ba9835884bfc99ad6a
alesscol7 <alessia.columban@student.howest.be>
2025-01-06 19:19:04 +0100

    all sliced videos

--- Athletes/KeypointDetection/try3.py
+++ Athletes/KeypointDetection/try3.py
@@ -6,8 +6,8 @@ from ultralytics import YOLO
 model = YOLO("yolov8s-pose.pt")  # Replace with your model path if different
 
 # Define the path to the video file and output folder
-video_path = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/YouTubeDownload/DownloadedVideos/Opdracht film techniek atletiek.mp4"
-output_video_folder = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/KeypointDetection/cutvideos/Opdracht film techniek atletiek"
+video_path = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/YouTubeDownload/DownloadedVideos/Video Techniek Atletiek.mp4"
+output_video_folder = "/Users/alessiacolumban/Desktop/TeamProject-GradindSysAthletes/Athletes/KeypointDetection/cutvideos/Video Techniek Atletiek"
 # Ensure output folder exists
 os.makedirs(output_video_folder, exist_ok=True)
 
